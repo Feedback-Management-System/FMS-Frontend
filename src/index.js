ef9bbea5599800936103c196f1b75b116f8835e3
tanishcq <tanishcqmehta@gmail.com>
2022-11-20 03:11:42 +0530

    Implemented react-router 6.4

--- src/index.js
+++ src/index.js
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+import { 
+  AdminHomePage, 
+} from 'pages/admin';
+
+import {
+  Sidebar,
+} from 'components';
+
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
+  Routes,
+  Link,
+  BrowserRouter,
+  Outlet,
 } from "react-router-dom";
-import App from './App';
+
+import { LandingPage } from 'pages';
+
+const AppLayout = () => (
+  <>
+    <Sidebar />
+    <Outlet />
+  </>
+)
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
-  },
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <AdminHomePage />
+        ),
+      },
+      {
+        path: "land",
+        element: <LandingPage />,
+      }
+    ]
+  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
 
 
