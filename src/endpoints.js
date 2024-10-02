c6928d88f919701154c3738a9861891f551df955
Ayush Pun <ayushpun504@gmail.com>
2024-10-03 01:20:42 +0530

    Backend url updated

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,1 +1,2 @@
-export const restUrl = 'https://naughty-mite-dirndl.cyclic.app/';
+// export const restUrl = 'https://naughty-mite-dirndl.cyclic.app/';
+export const restUrl = 'https://fms-backend-2vj6.onrender.com';     //New backend URL
